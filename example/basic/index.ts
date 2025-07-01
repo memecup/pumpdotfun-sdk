3b1c001cc5ef449ffd6d398c08820566e30c0b8a
memecup <misk.rhayour2@gmail.com>
2025-07-01 19:17:07 +0200

    Update index.ts

--- example/basic/index.ts
+++ example/basic/index.ts
@@ -1,43 +1,28 @@
 import dotenv from "dotenv";
 import { Keypair } from "@solana/web3.js";
 import { PumpFunSDK } from "../../src";
-import fs from "fs";
-import { File } from "fetch-blob/file.js";
-
 dotenv.config();
 
-// ----- CONFIG -------
 const secretKey = Uint8Array.from(JSON.parse(process.env.PRIVATE_KEY!));
 const creator = Keypair.fromSecretKey(secretKey);
 const mint = Keypair.generate();
 
-const imagePath = "example/basic/random.png";
+const meta = {
+  name: "Univers",
+  symbol: "UNV",
+  description: "Token test univers",
+  image: "https://gateway.pinata.cloud/ipfs/bafkreidgko6n7r2va2zjbzp4qaj6ybb4vrxafukknkl7qcnbyhn7pxejty"
+};
+
+const buyAmountSol = 0.005;
+const slippage = 500n;
 
 (async () => {
-  let fileBuffer: Buffer | undefined = undefined;
-  let file: any = undefined;
-  if (fs.existsSync(imagePath)) {
-    fileBuffer = fs.readFileSync(imagePath);
-    file = new File([fileBuffer], "logo.png", { type: "image/png" });
-    console.log("✅ Image trouvée pour le mint !");
-  }
-
   const sdk = new PumpFunSDK({
     rpc: process.env.HELIUS_RPC_URL!,
     payer: creator,
   });
 
-  const meta = {
-    name: "Univers",
-    symbol: "UNV",
-    description: "Token test univers",
-    file: file, // Polyfill File object
-  };
-
-  const buyAmountSol = 0.005;
-  const slippage = 500n;
-
-  console.log("⏳ Mint + buy du token...");
   const result = await sdk.createAndBuy(
     creator,
     mint,
