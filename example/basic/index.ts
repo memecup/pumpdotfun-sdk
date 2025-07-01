8dc3a86c291cb6d9aafbf9fd9408b45988dbd2b4
memecup <misk.rhayour2@gmail.com>
2025-07-01 22:50:02 +0200

    Update index.ts

--- example/basic/index.ts
+++ example/basic/index.ts
@@ -2,29 +2,36 @@ import dotenv from "dotenv";
 import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { DEFAULT_DECIMALS, PumpFunSDK } from "pumpdotfun-sdk";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import { AnchorProvider } from "@coral-xyz/anchor";
 import {
   getOrCreateKeypair,
   getSPLBalance,
   printSOLBalance,
-  printSPLBalance
-} from "../util.ts";
+  printSPLBalance,
+} from "../util.ts"; // Chemin correct si util.ts est dans example/
 
 dotenv.config();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const KEYS_FOLDER = __dirname + "/.keys"
+// Correction pour __dirname en module ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const KEYS_FOLDER = join(__dirname, "../.keys");
 const SLIPPAGE_BASIS_POINTS = 100n;
 
 const getProvider = () => {
   if (!process.env.HELIUS_RPC_URL) {
     throw new Error("Please set HELIUS_RPC_URL in .env file");
   }
-
-  const connection = new Connection(process.env.HELIUS_RPC_URL || "");
-  const wallet = new NodeWallet(new Keypair());
+  const connection = new Connection(process.env.HELIUS_RPC_URL);
+  // Utilise NodeWallet avec un vrai keypair du .env
+  const wallet = new NodeWallet(
+    Keypair.fromSecretKey(
+      Uint8Array.from(JSON.parse(process.env.PRIVATE_KEY!))
+    )
+  );
   return new AnchorProvider(connection, wallet, { commitment: "finalized" });
 };
 
@@ -33,7 +40,7 @@ const createAndBuyToken = async (sdk, testAccount, mint) => {
     name: "TST-7",
     symbol: "TST-7",
     description: "TST-7: This is a test token",
-    filePath: "example/basic/random.png", // tu as bien ce fichier dans le dossier
+    filePath: join(__dirname, "logo.png"), // Mets bien le fichier dans basic/
   };
 
   const createResults = await sdk.createAndBuy(
@@ -50,9 +57,10 @@ const createAndBuyToken = async (sdk, testAccount, mint) => {
 
   if (createResults.success) {
     console.log("Success:", `https://pump.fun/${mint.publicKey.toBase58()}`);
-    printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey);
+    await printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey);
   } else {
     console.log("Create and Buy failed");
+    console.log(createResults);
   }
 };
 
@@ -69,10 +77,14 @@ const buyTokens = async (sdk, testAccount, mint) => {
   );
 
   if (buyResults.success) {
-    printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey);
-    console.log("Bonding curve after buy", await sdk.getBondingCurveAccount(mint.publicKey));
+    await printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey);
+    console.log(
+      "Bonding curve after buy",
+      await sdk.getBondingCurveAccount(mint.publicKey)
+    );
   } else {
     console.log("Buy failed");
+    console.log(buyResults);
   }
 };
 
@@ -98,10 +110,14 @@ const sellTokens = async (sdk, testAccount, mint) => {
 
     if (sellResults.success) {
       await printSOLBalance(sdk.connection, testAccount.publicKey, "Test Account keypair");
-      printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey, "After SPL sell all");
-      console.log("Bonding curve after sell", await sdk.getBondingCurveAccount(mint.publicKey));
+      await printSPLBalance(sdk.connection, mint.publicKey, testAccount.publicKey, "After SPL sell all");
+      console.log(
+        "Bonding curve after sell",
+        await sdk.getBondingCurveAccount(mint.publicKey)
+      );
     } else {
       console.log("Sell failed");
+      console.log(sellResults);
     }
   }
 };
@@ -118,7 +134,7 @@ const main = async () => {
     await printSOLBalance(connection, testAccount.publicKey, "Test Account keypair");
 
     const globalAccount = await sdk.getGlobalAccount();
-    console.log(globalAccount);
+    console.log("Global Account:", globalAccount);
 
     const currentSolBalance = await connection.getBalance(testAccount.publicKey);
     if (currentSolBalance === 0) {
